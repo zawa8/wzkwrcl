4a22f90e87c9d6341796525d55b8fca1fb7221ad
viml kumar <vimalkg@gmail.com>
2025-04-29 12:46:45 +0530

    wzk.vercel.app mck82.vercel.app fb.com/ztrabc x.com/etphor

--- src/app/page.js
+++ src/app/page.js
@@ -18,10 +18,18 @@ steps to use : 1. pls replace ԃis tekst to indiα/nepαl/sinhl/bαnglα.
 1.1 to copy unicode google search : wiki indiα in indian languages
 2. pls press eu2i btn.
 3. result : tekst areα down.`;	
+  const wzktr = new wzk();
   const [iteksta, set_iteksta] = useState("");
   const handle_ita_change = (event) => { set_iteksta(event.target.value); };
+  function on_kh2uK() {
+	  let all_output  = wzktr.kh2hindiK(iteksta);
+	  set_iteksta(all_output);
+  }
+  function on_xvjqK2u() {
+	  let all_output  = wzktr.xvjqK2hindi(iteksta);
+	  set_iteksta(all_output);
+  }
   function on_eu2l() {  //alert("on_eu2l");
-    const wzktr = new wzk();
     let ioft= { i: iteksta, f: "both", t: "all",
 		o: {
 		  inglish: "", korean: "", russian: "", hindi: "", bangla: "", gurmukhi: "", guzrati: "",
@@ -29,7 +37,6 @@ steps to use : 1. pls replace ԃis tekst to indiα/nepαl/sinhl/bαnglα.
 		},
     };
     wzktr.ioft_tr(ioft);
-    //const keys = Object.keys(ioft.o);
     let all_output  = "";
     Object.keys(ioft.o).forEach(key => {
       all_output += `modern_${key}: ` + ioft.o[key] + " \n" ;
@@ -37,24 +44,17 @@ steps to use : 1. pls replace ԃis tekst to indiα/nepαl/sinhl/bαnglα.
     set_iteksta(all_output);
 }
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-4 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-      <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <Hsciifontpicker/>
-          <a
-            className="rounded-full border border-solid border-black dark:border-white transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://github.com/zawa8/font"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            free hscii fonts
-          </a>
-        </div>
+      <div className="flex gap-4 items-center flex-col sm:flex-row"><Hsciifontpicker/></div>
         <div className="flex gap-2 items-center flex-col sm:flex-row">
-          <Button color="default" variant="solid" onPress={on_eu2l}>ztr</Button>
+			<Button className="bg-green-500 text-white" onPress={on_eu2l}>ztr</Button>
+			<Button className="border-black border-2" onPress={on_xvjqK2u}>xvjqK2u</Button>
+			<Button className="border-black border-2" onPress={on_kh2uK}>kh2uK</Button>
+			<a className="border-black border-2" href="https://github.com/zawa8/font" target="_blank" rel="noopener noreferrer" > free hscii fonts </a>
         </div>
         <div className="flex min-w-full gap-2 items-center flex-col sm:flex-row">
-          <Textarea id="ita"  rows={12} className="min-w-full" value={iteksta}  onChange={handle_ita_change} placeholder={iplaceholer} />
+          <Textarea id="ita"  rows={12} className="min-w-full border-black" value={iteksta}  onChange={handle_ita_change} placeholder={iplaceholer} />
         </div>
       </main>
     </div>
